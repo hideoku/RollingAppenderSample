1318cd33b6f68667746a57758614eee71de96267
mas0061 <mas0061@gmail.com>
2012-06-30 23:49:20 +0900

    ログ切り替えをメソッド化してみた

--- src/sample/SampleRunner.java
+++ src/sample/SampleRunner.java
@@ -6,22 +6,26 @@ import org.apache.commons.logging.LogFactory;
 public class SampleRunner {
 
     private static final Log logger = LogFactory.getLog(SampleRunner.class);
-    
+
     public static void main(String[] args) {
         new SampleRunner().execute();
     }
-    
+
     private void execute() {
         logger.info("start execute()");
-        
+
         logger.info("ローテーション直前1");
-        logger.info("手動ローテーションするよ。");
+        rotate();
         logger.info("ローテーション直後1");
 
         logger.info("ローテーション直前2");
-        logger.info("手動ローテーションするよ。");
+        rotate();
         logger.info("ローテーション直後2");
 
         logger.info("end execute()");
+    }
+
+    private void rotate() {
+      logger.info("手動ローテーションするよ。");
     }
 }
